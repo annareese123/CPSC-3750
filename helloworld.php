cbd85279084d74dfd2dc9602c4e1c9a15cc6b5ab
JSwanier <jswanier@gmail.com>
2025-05-31 17:50:13 -0400

    Website with latest changes
    includes new navbar file, php inclusion, along with new CSS elements displayed

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World PHP</title>
+</head>
+<body>
+
+<?php include 'navbar.html'; ?>
+
+<h1>Hello, World!</h1>
+
+</body>
+</html>
